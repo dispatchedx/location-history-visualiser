a71a3a2c6a44beb22be79d7dc16fbee4fd125c54
dispatchedx <dispatchedx@outlook.com>
2024-06-25 22:39:16 +0300

    added a way to generate test data (previously in challenges repo)

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # location-history-visualiser
 A web app with users being able to upload a google-take out location history file and see some statistics, graphs and a heatmap.
 For explanation and screenshots see: [Presentation](/presentation.pdf) (greek)
-Database creation at: /database export.
+Database creation at: /database export. Fake/test data can be generated with generate_random_location_dates.py
 
 <b>note:</b> data outside of [patras, Greece] is cut off as per assignment guidelines
 
---
+++ generate_random_location_dates.py
@@ -0,0 +1,24 @@
+import random
+from random import randrange
+data = '{ "locations" :['
+# This tool was made to generate fake/test data for https://github.com/dispatchedx/location-history-visualiser
+for i in range(15000):
+
+    start_timestamp=1420070400000 #2015/8/25
+    end_timestamp=1603843200000 # 2020/10/28
+    #1598358416123 #2020/8/25
+    timestamp=random.randint(start_timestamp ,end_timestamp )
+
+    activities=["WALKING", "STILL", "ON_BICYCLE", "RUNNING", "IN_VEHICLE"]
+    activity=activities[randrange(5)]
+
+    data = data + '{ "timestampMs" : "'+str(timestamp)+'","latitudeE7" : 381852128,"longitudeE7" : 217105715,"accuracy" : 64,"activity" : [ {"timestampMs" : "'+str(timestamp)+'","activity" : [ {"type" : "'+activity+'", "confidence" : 100 } ] } ]},' #removing this last traling comma when it finishes
+
+
+data = data[:-1] + "]}"
+
+with open('lulw.json', 'w') as file:
+    file.write(data)
+
+
+
